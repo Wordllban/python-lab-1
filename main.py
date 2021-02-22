71ca33e469dec5385805356da00cbbfe18e1ab2b
Wordllban <mininkov3228@gmail.com>
2021-02-22 12:12:30 +0200

    Updated after code review

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 class Credit_card:
     card_size = "default"
+    __bank = None
+    __user_name = None
+    __numbers = None
+    __cvv = None
+    __card_type = None
+    __currency = None
 
-    def __init__(self, bank=None, user_name=None, numbers=None, cvv=None, card_type=None, currency=None):
-        self.bank = bank
-        self.user_name = user_name
-        self.numbers = numbers
-        self.cvv = cvv
-        self.card_type = card_type
-        self.currency = currency
+
+    def __init__(self, bank = None, user_name = None, numbers = None, cvv = None, card_type = None, currency = None):
+        self.__bank = bank
+        self.__user_name = user_name
+        self.__numbers = numbers
+        self.__cvv = cvv
+        self.__card_type = card_type
+        self.__currency = currency
+
 
     def __del__(self):
         return
 
     def __str__(self):
-        return f"Bank: {self.bank}\n" \
-               f"User name: {self.user_name}\n" \
-               f"Numbers: {self.numbers}\n" \
-               f"CVV: {self.cvv}\n" \
-               f"Card type: {self.card_type}\n" \
-               f"Currency: {self.currency}"
+        return f"Bank: {self.__bank}\n" \
+               f"User name: {self.__user_name}\n" \
+               f"Numbers: {self.__numbers}\n" \
+               f"CVV: {self.__cvv}\n" \
+               f"Card type: {self.__card_type}\n" \
+               f"Currency: {self.__currency}"
 
     @staticmethod
     def get_card_size():
@@ -26,10 +34,10 @@ class Credit_card:
 
 
 def main():
-    card_1 = Credit_card("Privat24", "Zenovii Veres", "1337228322777666", "777", "Wood", "Shekeli")
-    card_2 = Credit_card("Privat24", "Zenovii Veres", "Shekeli")
+    card_1 = Credit_card("Privat24", "ZenoviiVeres", "1337228322777666", "777", "Wood", "Shekeli")
+    card_2 = Credit_card("Privat24", "ZenoviiVeres", "Shekeli")
     card_3 = Credit_card()
-    print(card_2)
+    print(card_1)
 
 
 if __name__ == '__main__':
